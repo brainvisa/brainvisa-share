b1673cd4a8494dc568af8f80ac17a659cfad6730
denisri <denis.riviere.gm@gmail.com>
2016-09-05 14:55:35 +0000

    compatibility with python3
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/brainvisa-share/branches/bug_fix@74366 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- fast_brainvisa-share_copy.py
+++ fast_brainvisa-share_copy.py
@@ -1,5 +1,9 @@
+from __future__ import print_function
 import sys, os, platform
-from itertools import izip
+if sys.version_info[0] >= 3:
+    izip = zip
+else:
+    from itertools import izip
 
 relativeFiles= []
 sourceFiles= []
@@ -28,30 +32,31 @@ while stack:
     sourceFiles.append( fullPath )
     destinationFiles.append( destinationPath )
 
-print 'set( relativeFiles'
-print ' ', '\n  '.join( ( '"' + i + '"' for i in relativeFiles ) )
-print ')'
-print
-print 'set( sourceFiles'
-print ' ', '\n  '.join( ( '"' + i + '"' for i in sourceFiles ) )
-print ')'
-print
-print 'set( destinationFiles'
-print ' ', '\n  '.join( ( '"' + i + '"' for i in destinationFiles ) )
-print ')'
-print
+print('set( relativeFiles')
+print(' ', '\n  '.join( ( '"' + i + '"' for i in relativeFiles ) ))
+print(')')
+print()
+print('set( sourceFiles')
+print(' ', '\n  '.join( ( '"' + i + '"' for i in sourceFiles ) ))
+print(')')
+print()
+print('set( destinationFiles')
+print(' ', '\n  '.join( ( '"' + i + '"' for i in destinationFiles ) ))
+print(')')
+print()
 for source, dest, relative in izip( sourceFiles, destinationFiles, relativeFiles ):
   destinationPath = os.path.join( destination, os.path.dirname( relative ) )
   if os.path.sep != '/' :
     destinationPath = destinationPath.replace( os.path.sep, '/' )
-  print 'if ( UNIX OR APPLE )'
-  print '  add_custom_command( OUTPUT "' + dest +'"'
-  print '                      COMMAND "' + cmake + '" -E make_directory "' + os.path.dirname( dest ) + '"'
-  print '                      COMMAND "' + cmake + '" -E create_symlink "' + source + '" "' + dest + '" )'
-  print 'else()'
-  print '  add_custom_command( OUTPUT "' + dest +'"'
-  print '                      COMMAND "' + cmake + '" -E copy_if_different "' + source + '" "' + dest + '" )'
-  print 'endif()'
-  print 'BRAINVISA_INSTALL( PROGRAMS "' + source +'"'
-  print '                   DESTINATION "'+ destinationPath +'"'
-  print '                   COMPONENT ${PROJECT_NAME} )'
+  print('if ( UNIX OR APPLE )')
+  print('  add_custom_command( OUTPUT "' + dest +'"')
+  print('                      COMMAND "' + cmake + '" -E make_directory "' + os.path.dirname( dest ) + '"')
+  print('                      COMMAND "' + cmake + '" -E create_symlink "' + source + '" "' + dest + '" )')
+  print('else()')
+  print('  add_custom_command( OUTPUT "' + dest +'"')
+  print('                      COMMAND "' + cmake + '" -E copy_if_different "' + source + '" "' + dest + '" )')
+  print('endif()')
+  print('BRAINVISA_INSTALL( PROGRAMS "' + source +'"')
+  print('                   DESTINATION "'+ destinationPath +'"')
+  print('                   COMPONENT ${PROJECT_NAME} )')
+
